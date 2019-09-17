8d543afcf30916984f0901f0cf411fed07962d8e
projojoboy <joeykossen1@gmail.com>
2019-09-17 14:41:33 +0200

    Add Project

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.SetResolution((int)Screen.width, (int)Screen.height, true);
+    }
+}
